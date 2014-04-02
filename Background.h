f7369552ad76e295770eec9fb8b424ac9a4b4fc8
jrichelsen <jrichelsen@gmail.com>
2014-04-02 16:33:53 -0400

    Standardize Background.h

--- Background.h
+++ Background.h
@@ -1,13 +1,24 @@
+/*
+Bill Gowans, Jack Magiera, Jon Richelsen
+CSE20212
+1943_Project
+Background.h
+	Interface of background class (displays scrolling background)
+History
+	03/31/14	Bill Gowans		Create
+	04/02/14	Jon Richelsen	Standardize
+To Do
+*/
 #ifndef BACKGROUND_H
 #define BACKGROUND_H
 
-using namespace std;
+#include<string>
 
 class Background {
 	public:
-		Background (string);
+		Background(string);
 	private:
 		string fileName;
 };
 
-#endif
+#endif //BACKGROUND_H
