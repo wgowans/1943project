bca70143c4da19c29a9f452f801f0b1a024ce396
jrichelsen <jrichelsen@gmail.com>
2014-04-08 20:45:53 -0400

    Add test driver for Sprite, finish Sprite header and source files

---
+++ spriteTestMain.cpp
@@ -0,0 +1,18 @@
+/*
+Bill Gowans, Jack Magiera, Jon Richelsen
+CSE20212
+1943_Project
+spriteTestMain.cpp
+	Driver to test Sprite class
+History
+	04/08/14	Jon Richelsen	Create
+To Do
+*/
+#include"SDL/SDL.h"
+#include"Sprite.h"
+#include"SpriteMappings.h"
+
+int main() {
+	Sprite spr_plyr_hlthy_straight(sprites.png, rect_plyr_hlthy_straight);
+	
+}
