dc91f61faf1b2b8a1f705c5b72540a2319892b90
jrichelsen <jrichelsen@gmail.com>
2014-04-16 16:01:23 -0400

    Remove To Do section from template source file

--- sourceTemplate.cpp
+++ sourceTemplate.cpp
@@ -7,9 +7,6 @@ Player.cpp
 History
 	XX/XX/XX	Person's name	Summary of edits made
 	XX/XX/XX	Person's name	Summary of edits made
-To Do
-	To do item 1
-	To do item 2
 BUT SERIOUSLY, THE ACTUAL HISTORY OF THIS TEMPLATE FILE
 	04/01/14	Jon Richelsen	Create file
 	04/02/14	Jon Richelsen	Add "To Do" section
