dd98b9e017e92461fc90007b6c37982579f98c61
jrichelsen <jrichelsen@gmail.com>
2014-04-16 15:59:25 -0400

    Standardize Enemy header and source files, fix typo in Counter source file

---
+++ Enemy.cpp
@@ -0,0 +1,12 @@
+/*
+Bill Gowans, Jack Magiera, Jon Richelsen
+CSE20212
+1943_Project
+Enemy.cpp
+	Implementation of Enemy class (base class for derived enemy classes)
+History
+	03/31/14	Bill Gowans		Create
+	04/16/14	Jon Richelsen	Standardize
+To Do
+	Completely revamp class
+*/
--- Enemy.h
+++ Enemy.h
@@ -1,23 +1,36 @@
+/*
+Bill Gowans, Jack Magiera, Jon Richelsen
+CSE20212
+1943_Project
+Enemy.h
+	Interface of Enemy class (base class for derived enemy classes)
+History
+	03/31/14	Bill Gowans		Create, declare default constructor, forward(), slightR(), sharpR(), slightL(), sharpR(), getX(), getY(), getXVel(), getYVel(), and private variable healthCntr
+	04/16/14	Jon Richelsen	Standardize
+To Do
+	Completely revamp class
+*/
 #ifndef ENEMY_H
 #define ENEMY_H
 
-using namespace std;
+#include<string>
+#include"Counter.h"
+#include"Sprite.h"
 
 class Enemy {
 	public:
 		Player ();
-		Sprite for (string, string);
-		Sprite slightR (string, int, int);
-		Sprite sharpR (string, int, int);
-		Sprite slightL (string, int, int);
-		Sprite sharpR (string, int, int);
-		Counter healthCntr;
-		int getX ();
-		int getY ();
-		int getXVel ();
+		Sprite forward(string, string);
+		Sprite slightR(string, int, int);
+		Sprite sharpR(string, int, int);
+		Sprite slightL(string, int, int);
+		Sprite sharpR(string, int, int);
+		int getX();
+		int getY();
+		int getXVel();
 		int getYVel();		
 	private:
-
+		Counter healthCntr;
 };
 
-#endif
+#endif //ENEMY_H
